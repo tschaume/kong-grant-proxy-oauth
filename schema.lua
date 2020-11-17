0ce4afa4dec79af84cda8ccaef856283c2df3385
Patrick Huck <phuck@lbl.gov>
2020-11-16 18:26:19 -0800

    kong-grant: random secret, anonymous in schema

--- schema.lua
+++ schema.lua
@@ -1,6 +1,18 @@
 -- Copyright 2020 Patrick Huck
-
 local typedefs = require "kong.db.schema.typedefs"
+local utils = require "kong.tools.utils"
+local encode_base64 = ngx.encode_base64
+local char = string.char
+local rand = math.random
+
+--- kong.utils.random_string with 32 bytes instead
+-- @returns random string of length 44
+local function random_string()
+    return encode_base64(utils.get_rand_bytes(32, true))
+    :gsub("/", char(rand(48, 57)))  -- 0 - 10
+    :gsub("+", char(rand(65, 90)))  -- A - Z
+    :gsub("=", char(rand(97, 122))) -- a - z
+end
 
 return {
     name = "grant-proxy-oauth",
@@ -11,8 +23,8 @@ return {
             fields = {
                 {host = typedefs.url {default = "http://localhost:3000"}},
                 {provider = {type = "string", default = "phantauth"}},
-                {secret = {type = "string", required = true}},
-                {auth_token_expire_time = {type = "integer", default = 259200}},
+                {secret = {type = "string", required = false, default = random_string()}},
+                {anonymous = {type = "string", uuid = true}},
             },
         }},
     },
